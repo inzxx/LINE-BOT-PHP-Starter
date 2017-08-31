f64fddf510b891fe751bc4f5bc2f7271a0089b52
inzxx <aa_anger@me.com>
2017-08-31 13:44:20 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -19,7 +19,8 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => $text ,
+				"ชืออะไร" => "เราชื่อวิทยาศาสตร์ตัวน้อย" ,
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
